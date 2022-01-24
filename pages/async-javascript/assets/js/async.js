863866e53a697d6f36b13fe99346753bb8a302e3
Mittzera <hugocavalcantesr@gmail.com>
2022-01-24 14:39:49 -0300

    added a async exercise

---
+++ pages/async-javascript/assets/js/async.js
@@ -0,0 +1,23 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search/';
+const catBtn = document.getElementById('change-cat');
+const catImg = document.getElementById('cat');
+
+const getCats = async() => {
+
+    try{
+        
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+    return json.url;
+    } catch(e){
+        console.log(e.message);
+    }
+}
+
+const loadImg = async () => {
+    catImg.src = await getCats();
+}
+
+catBtn.addEventListener('click', loadImg());
+
+loadImg();
